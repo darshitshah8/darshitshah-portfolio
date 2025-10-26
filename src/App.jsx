b2a75de1795af855cdb2c502db8db95ef3c6bcba
Darshit Shah <136233486+darshitshah8@users.noreply.github.com>
2025-10-26 18:15:50 +0000

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h1 className=''>Darshit Shah</h1>
+        <p>Software Developer</p>
+        <button className='button mt-2'>
+          <a href='https://www.linkedin.com/in/darshitshah8/'>
+          Reach Out
+          </a>
+          </button>
+      </div>
+    </>
+  )
+}
+
+export default App
